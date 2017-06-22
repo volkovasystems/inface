dc0f8f0e58664408be6814a42c9581a9cacfb4ff
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-22 16:08:34 +0800

    Save changes.

--- inface.js
+++ inface.js
@@ -35,6 +35,10 @@
 			"module": "inface",
 			"author": "Richeve S. Bebedor",
 			"eMail": "richeve.bebedor@gmail.com",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
+				"Vinse Vinalon <vinsevinalon@gmail.com>"
+			],
 			"repository": "https://github.com/volkovasystems/inface.git",
 			"test": "inface-test.js",
 			"global": true
